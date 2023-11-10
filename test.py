f73725e8453898d6b9ea564a15a413d8290cec4c
BrasD99 <brasd99@gmail.com>
2023-11-10 18:31:32 +0300

    Removed unnecessary script

--- test.py
+++
@@ -1,8 +0,0 @@
-from TTS.api import TTS
-import torch
-
-device = torch.device('cpu')
-
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
-
-tts.tts_to_file(text="Hello world!", speaker_wav="/Users/brasd99/Music/Music/Media.localized/Music/Unknown Artist/Unknown Album/tmp2zhg2a1w.wav", language="ru", file_path="output.wav")
